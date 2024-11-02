c88b820e1adbb0736df3389d9a639d531ac0d3da
rod-nickle <rod@bitwise-it.com>
2024-11-01 18:13:16 -0600

    Intro to Trees: Vampr!

---
+++ scratchpad.js
@@ -0,0 +1,43 @@
+const Vampire = require("./vampire");
+
+original = new Vampire("Original", 1900);
+ansel = new Vampire("Ansel", 1930);
+bart = new Vampire("Bart", 1932);
+elgort = new Vampire("Elgort", 1960);
+sarah = new Vampire("Sarah", 1962);
+andrew = new Vampire("Andrew", 2000);
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+elgort.addOffspring(andrew);
+
+// console.log("Orginal: ", original);
+// console.log("Ansel: ", ansel);
+// console.log("Elgort: ", elgort);
+
+// console.log("Orginal: ", original.numberOfOffspring);
+// console.log("Ansel: ", ansel.numberOfOffspring);
+// console.log("Elgort: ", elgort.numberOfOffspring);
+
+// console.log("Orginal: ", original.numberOfVampiresFromOriginal);
+// console.log("Ansel: ", ansel.numberOfVampiresFromOriginal);
+// console.log("Elgort: ", elgort.numberOfVampiresFromOriginal);
+
+// console.log("Orginal and Ansel ", original.isMoreSeniorThan(ansel));
+// console.log("Ansel and Bart: ", ansel.isMoreSeniorThan(bart));
+// console.log("Elgort and Ansel: ", elgort.isMoreSeniorThan(ansel));
+// console.log("Bart and Andrew: ", bart.isMoreSeniorThan(andrew));
+
+// * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
+// * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+// console.log("Ansel and Ansel: ", ansel.closestCommonAncestor(ansel));
+// console.log("Ansel and Sarah: ", ansel.closestCommonAncestor(sarah));
+// console.log("Ansel and Andrew: ", ansel.closestCommonAncestor(andrew));
+console.log("Ansel and Bart: ", ansel.closestCommonAncestor(bart));
+console.log("Sarah and Bart: ", sarah.closestCommonAncestor(bart));
+ 
+// console.log("Orginal: ", original.getAncestors());
+// console.log("Ansel: ", ansel.getAncestors());
+// console.log("Elgort: ", elgort.getAncestors());
--- vampire.js
+++ vampire.js
@@ -10,25 +10,40 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until the original (no parent) is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const thisVampireNumberOfVampires = this.numberOfVampiresFromOriginal;
+    const otherVampireNumberOfVampires = vampire.numberOfVampiresFromOriginal;
 
+    if (thisVampireNumberOfVampires < otherVampireNumberOfVampires) {
+      return true;
+    }
+    return false;
   }
 
-  /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
   // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
@@ -36,7 +51,26 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    // If they are the same vampire, exit immediately.
+    if (this.name === vampire.name) {
+      return this;
+    }
 
+    let thisCurrentVampire = this;
+  
+
+    // Go up the tree for this vampire.
+    while (thisCurrentVampire) {
+      // Go up the tree for the other vampire.
+      let otherCurrentVampire = vampire;
+      while (otherCurrentVampire) {
+        if (thisCurrentVampire.name === otherCurrentVampire.name) {
+          return thisCurrentVampire;
+        }
+        otherCurrentVampire = otherCurrentVampire.creator;
+      }
+      thisCurrentVampire = thisCurrentVampire.creator;
+    }
   }
 }
 
